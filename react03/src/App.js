9fe5aaca6c5ad2c27ef5686ef69115a4762ecb43
rintarouchida <r.uchida311@gmail.com>
2022-03-27 18:12:48 +0900

    practice_react05

--- react03/src/App.js
+++ react03/src/App.js
@@ -2,26 +2,31 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  let items = [
-    {"name": "イチゴ", "price": "100"},
-    {"name": "りんご", "price": "150"},
-    {"name": "バナナ", "price": "230"},
-  ]
-
   return (
-    <div className="App container">
-      <table className="table table-striped">
-        <tbody>
-          {items.map((value)=>(
-          <tr>
-            <th scope="row">{value.name}</th>
-          <td>{value.price}円</td>
-          </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="container text-center">
+      <Clock/>
     </div>
   )
+}
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.now = new Date();
+    this.state = {
+      time: `${this.now.getHours()}` + ':' + `${this.now.getMinutes()}` + ':' + `${this.now.getSeconds()}`
+    }
+    this.refresh = this.refresh.bind(this);
+  }
+  refresh(){
+    this.now = new Date();
+    this.setState((state)=>({
+      time: `${this.now.getHours()}` + ':' + `${this.now.getMinutes()}` + ':' + `${this.now.getSeconds()}`
+    }));
+  }
+  render(){
+    return <h1 onClick={this.refresh}>{this.state.time}</h1>
+  }
 }
 
 export default App;
